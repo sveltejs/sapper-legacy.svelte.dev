c8669293c4bfd761f5a8f4bcfa7e50f603730956
Rich Harris <richard.a.harris@gmail.com>
2017-12-25 15:49:30 -0500

    add toast for new service worker

--- templates/main.js
+++ templates/main.js
@@ -2,3 +2,13 @@ import { init } from 'sapper/runtime/app.js';
 
 // `routes` is an array of route objects injected by Sapper
 init(document.querySelector('#sapper'), __routes__);
+
+if (navigator.serviceWorker && navigator.serviceWorker.controller) {
+	navigator.serviceWorker.controller.onstatechange = function(e) {
+		if (e.target.state === 'redundant') {
+			import('./components/Toast.html').then(mod => {
+				mod.default.show();
+			});
+		}
+	};
+}
