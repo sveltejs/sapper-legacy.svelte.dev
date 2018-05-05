83f1c8a276678357aab62dcfc2718da1f7777758
Rich Harris <richard.a.harris@gmail.com>
2018-05-05 08:57:46 -0400

    dogfood new layout stuff

--- app/server.js
+++ app/server.js
@@ -2,12 +2,20 @@ import express from 'express';
 import compression from 'compression';
 import sapper from 'sapper';
 import serve from 'serve-static';
+import { Store } from 'svelte/store.js';
 import { routes } from './manifest/server.js';
+import App from './App.html';
 
 express()
 	.use(
 		compression({ threshold: 0 }),
 		serve('assets'),
-		sapper({ routes })
+		sapper({
+			App,
+			routes,
+			store: () => new Store({
+				guide_contents: []
+			})
+		})
 	)
 	.listen(process.env.PORT);
